b1b3b0f55b8db0b3fac2d67e24fa4e864ecc1a7d
cduar <52426216+cduar@users.noreply.github.com>
2023-05-21 20:51:53 -0500

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Christopher Duarte on 5/21/23.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

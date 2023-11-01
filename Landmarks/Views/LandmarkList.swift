1225555f9412f8be3f21b604d8ac768a53d2150c
cduar <52426216+cduar@users.noreply.github.com>
2023-10-31 21:33:01 -0500

    First commit

---
+++ Landmarks/Views/LandmarkList.swift
@@ -0,0 +1,32 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by Christopher Duarte on 7/17/23.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        NavigationView{
+            List(landmarks) { landmark in
+                NavigationLink {
+                    LandmarkDetail(landmark: landmark)
+                } label:{
+                    LandmarkRow(landmark: landmark)
+                }
+            }
+            .navigationTitle("Landmarks")
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+       ForEach(["iPhone SE (2nd Generation)", "iPhone XS Max", "iPad Pro (12.9-inch) (2nd Generation)"], id:\.self){ deviceName in
+            LandmarkList()
+                .previewDevice(PreviewDevice(rawValue: deviceName))
+                .previewDisplayName(deviceName)}
+    }
+}
